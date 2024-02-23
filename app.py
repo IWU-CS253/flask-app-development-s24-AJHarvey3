39d98ae096cc4d2bb54efab65d29933782f27ee2
Alex Harvey <aharvey1@iwu.edu>
2024-02-23 09:22:41 -0600

    Add delete function

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def close_db(error):
 @app.route('/')
 def show_entries():
     db = get_db()
-    cur = db.execute('select title, text from entries order by id desc')
+    cur = db.execute('select id, title, text from entries order by id desc')
     entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
 
@@ -80,4 +80,12 @@ def add_entry():
                [request.form['title'], request.form['text']])
     db.commit()
     flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/delete', methods=['POST']
+def delete_task():
+    db = get_db()
+    row_id = request.form['task_id']
+    db.execute('delete from entries where id = ?', (row_id,))
+    db.commit()
     return redirect(url_for('show_entries'))
