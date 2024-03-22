b26ac461dee7363753834bd40c1151b1dd173b4e
Alex Harvey <aharvey1@iwu.edu>
2024-03-21 21:25:28 -0500

    trying to fix test

--- flaskr_test.py
+++ flaskr_test.py
@@ -62,7 +62,7 @@ class FlaskrTestCase(unittest.TestCase):
 
         # edit
         rv = self.app.post('/edit', data=dict(
-            entry_id=entry_id,
+            row_id=entry_id,
             title='Another Test Entry',
             text='waka waka',
             category='Another Category'
